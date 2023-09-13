e0f6949ee6c50c4b810adf6bb4ffb3ea3da9f591
antifragile <14090704+holy-motors@users.noreply.github.com>
2023-09-12 22:40:56 -0400

    add comment

--- app.py
+++ app.py
@@ -80,6 +80,7 @@ def check_availability():
         print("has dns")
         return jsonify({"available": False})
 
+    # FIXME: AttributeError: module 'whois' has no attribute 'parser'
     # try:
     #     print("does not have dns")
     #     w = whois.whois(domain_name)
